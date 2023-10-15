a32894c63eaa89b3b2f3615fbf58ef23a5909b07
theoslemos <112652872+theoslemosshu@users.noreply.github.com>
2023-10-15 16:42:14 -0500

    JavaScript commit!

--- main.js
+++ main.js
@@ -14,11 +14,27 @@ setTimeout(function() {
     loadingOverlay.style.opacity = "0"; // Cambia la opacidad a 0 para ocultar el overlay
     setTimeout(function() {
         loadingOverlay.style.display = "none"; // Oculta completamente el overlay
-    }, 200); // Espera 1 segundo (ajusta este valor según tus necesidades)
+    }, 200); 
 }, 0); // No esperar y ocultar inmediatamente
 
 // Carga la primera sección
 setTimeout(function() {
     const section1 = document.querySelector(".section1");
     section1.style.opacity = "1"; // Muestra la sección
-}, 200); // Espera 1 segundos antes de mostrar la sección
+}, 200);
+
+// Función para cambiar el fondo a la imagen con transición de difuminado
+function transitionToImageBackground() {
+    const body = document.body;
+
+    // Cambia el fondo de negro a la imagen deseada
+    body.style.backgroundImage = "url('https://i.imgur.com/prtQgf3.jpg')";
+
+    // Espera un momento antes de aplicar la transición
+    setTimeout(function() {
+        body.style.backgroundColor = "transparent"; // Cambia el fondo de negro a transparente
+    }, 200); 
+}
+
+
+transitionToImageBackground();
